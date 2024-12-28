f6c40be4dcaa295768e5c746139155672a248b6d
GG-Bond <2268574547@qq.com>
2024-12-28 22:04:33 +0800

    hot100

--- LeetCode_hot100.cpp
+++ LeetCode_hot100.cpp
@@ -6,24 +6,24 @@ https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=to
 你可以假设每种输入只会对应一个答案,并且你不能使用两次相同的元素
 你可以按任意顺序返回答案
 */
-//#include <iostream>
-//#include <vector>
-//#include <unordered_map>
-//using namespace std;
-//class Solution {
-//public:
-//    vector<int> twoSum(vector<int>& nums, int target)
-//    {
-//        unordered_map<int, int> hashMap;
-//        for (int i = 0; i < nums.size(); ++i)
-//        {
-//            unordered_map<int, int>::iterator it = hashMap.find(target - nums[i]);
-//            if (it != hashMap.end()) return { i, it->second };
-//            else hashMap[nums[i]] = i;
-//        }
-//        return {};
-//    }
-//};
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            unordered_map<int, int>::iterator it = hashMap.find(target - nums[i]);
+            if (it != hashMap.end()) return { i, it->second };
+            else hashMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
 
 
 
@@ -60,6 +60,115 @@ public:
 
 
 /*
+最长序列长度
+https://leetcode.cn/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题
 
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4
+*/
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+class Solution
+{
+public:
+    int longestConsecutive(vector<int>& nums)
+    {
+        unordered_set<int> hashSet;
+        for (size_t i = 0; i < nums.size(); ++i)
+            hashSet.insert(nums[i]);
+
+        int longest = 0;
+        for (auto& number : hashSet)
+        {
+            if (hashSet.count(number - 1)) continue;
+            else
+            {
+                int currentNumber = number;
+                int length = 1;
+                while (hashSet.count(currentNumber + 1)) {
+                    currentNumber += 1;
+                    length += 1;
+                }
+                longest = max(longest, length);
+                if (hashSet.size() == longest) break;
+            }
+        }
+        return longest;
+    }
+};
+
+
+
+/*
+移动零
+https://leetcode.cn/problems/move-zeroes/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序
+请注意，必须在不复制数组的情况下原地对数组进行操作
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+        int start = 0, end = 0;
+        while (end < nums.size())
+        {
+            if (0 != nums[end]) {
+                swap(nums[start], nums[end]);
+                ++start;
+            }
+            ++end;
+        }
+    }
+};
+
+
+
+/*
+盛水最多的容器
+https://leetcode.cn/problems/container-with-most-water/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水
+返回容器可以储存的最大水量
+说明：你不能倾斜容器
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    // 每次移动都是为取更多的水, 若短板不动, 取的水永远不会比上一次更多
+    int maxArea(vector<int>& height)
+    {
+        int left = 0, right = height.size() - 1;
+        int result = 0;
+        while (left < right)
+        {
+            int area = min(height[left], height[right]) * (right - left);
+            result = max(area, result);
+            if (height[left] <= height[right]) ++left;
+            else --right;
+        }
+        return result;
+    }
+};
+
+
+
+/*
+三数之和
+https://leetcode.cn/problems/3sum/description/?envType=study-plan-v2&envId=top-100-liked
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k
+同时还满足 nums[i] + nums[j] + nums[k] == 0
+请你返回所有和为 0 且不重复的三元组。
+注意：答案中不可以包含重复的三元组
 */
 
