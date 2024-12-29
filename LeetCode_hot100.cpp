9b13f75f3753936c583bc371dd3fdda89a61d720
GG-Bruse <BJY811@outlook.com>
2024-12-30 00:15:50 +0800

    hot100

---
+++ LeetCode_hot100.cpp
@@ -0,0 +1,206 @@
+/*
+两数之和
+https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个整数数组nums和一个整数目标值target,请你在该数组中找出和为目标值target的那两个整数
+并返回它们的数组下标
+你可以假设每种输入只会对应一个答案,并且你不能使用两次相同的元素
+你可以按任意顺序返回答案
+*/
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            unordered_map<int, int>::iterator it = hashMap.find(target - nums[i]);
+            if (it != hashMap.end()) return { i, it->second };
+            else hashMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
+
+/*
+字母异位词分组
+https://leetcode.cn/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-100-liked
+给你一个字符串数组，请你将字母异位词组合在一起。可以按任意顺序返回结果列表
+字母异位词是由重新排列源单词的所有字母得到的一个新单词
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+*/
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        unordered_map<string, vector<string>> hashMap;
+        for (string& str : strs) {
+            string tmp = str;
+            sort(tmp.begin(), tmp.end());
+            hashMap[tmp].push_back(str);
+        }
+        vector<vector<string>> result;
+        for (auto it = hashMap.begin(); it != hashMap.end(); ++it)
+            result.push_back(it->second);
+        return result;
+    }
+};
+
+
+
+/*
+最长序列长度
+https://leetcode.cn/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题
+
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4
+*/
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+class Solution
+{
+public:
+    int longestConsecutive(vector<int>& nums)
+    {
+        unordered_set<int> hashSet;
+        for (size_t i = 0; i < nums.size(); ++i)
+            hashSet.insert(nums[i]);
+
+        int longest = 0;
+        for (auto& number : hashSet)
+        {
+            if (hashSet.count(number - 1)) continue;
+            else
+            {
+                int currentNumber = number;
+                int length = 1;
+                while (hashSet.count(currentNumber + 1)) {
+                    currentNumber += 1;
+                    length += 1;
+                }
+                longest = max(longest, length);
+                if (hashSet.size() == longest) break;
+            }
+        }
+        return longest;
+    }
+};
+
+
+
+/*
+移动零
+https://leetcode.cn/problems/move-zeroes/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序
+请注意，必须在不复制数组的情况下原地对数组进行操作
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+        int start = 0, end = 0;
+        while (end < nums.size())
+        {
+            if (0 != nums[end]) {
+                swap(nums[start], nums[end]);
+                ++start;
+            }
+            ++end;
+        }
+    }
+};
+
+
+
+/*
+盛水最多的容器
+https://leetcode.cn/problems/container-with-most-water/description/?envType=study-plan-v2&envId=top-100-liked
+给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水
+返回容器可以储存的最大水量
+说明：你不能倾斜容器
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    // 每次移动都是为取更多的水, 若短板不动, 取的水永远不会比上一次更多
+    int maxArea(vector<int>& height)
+    {
+        int left = 0, right = height.size() - 1;
+        int result = 0;
+        while (left < right)
+        {
+            int area = min(height[left], height[right]) * (right - left);
+            result = max(area, result);
+            if (height[left] <= height[right]) ++left;
+            else --right;
+        }
+        return result;
+    }
+};
+
+
+
+/*
+三数之和
+https://leetcode.cn/problems/3sum/description/?envType=study-plan-v2&envId=top-100-liked
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k
+同时还满足 nums[i] + nums[j] + nums[k] == 0
+请你返回所有和为 0 且不重复的三元组。
+注意：答案中不可以包含重复的三元组
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        int size = nums.size();
+        sort(nums.begin(), nums.end()); // 由于不可重复, 需进行排序
+        vector<vector<int>> result;
+        for (size_t i = 0; i < size - 2; ++i)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;//需与上一次枚举的数不相同
+            int target = 0 - nums[i];
+            int k = size - 1;
+            for (int j = i + 1; j < size - 1; ++j)
+            {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;//需与上一次枚举的数不相同
+                while (j < k && nums[j] + nums[k] > target) --k;// 双指针查找, <、=都停止
+                // nums[j] < 所有while枚举过的nums[k], 若nums[j]增大, nums[k]需减小
+                // 但当j == k是, 说明找了所有的k, 都是nums[j] + nums[k] > target
+                // j继续向后只可能更大, 并且不可能有更小的nums[k]了
+                // 此时结束本次j的循环 
+                if (j == k) break;
+                if (nums[j] + nums[k] == target)
+                    result.push_back({ nums[i], nums[j], nums[k] });
+            }
+        }
+        return result;
+    }
+};
